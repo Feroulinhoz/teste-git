b2bdb5ae96c321489df29a3a8a6429a3abceeb65
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-12 09:24:00 -0300

    Enviando outra classe de logica aritmetica

--- src/Teste2.java
+++ src/Teste2.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Teste2 {
+
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numero =0;
 
         System.out.println("Sou feliz agora não não vá embora nãoooooooo");
+        System.out.println("DIgite seu número favorito:");
+        numero = entrada.nextInt();
+        numero = numero*2;
+        System.out.println("O dobro do seu numero é esse: "+numero);
     }
 }
